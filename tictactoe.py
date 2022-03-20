0eaf4dd87c59cfa1d4c813b45d41cbbfa6f6d7ea
Nadia Maltezou <maltezou.nadia@gmail.com>
2022-03-20 17:22:12 +0100

    Nadias first draft of tictactoe

---
+++ tictactoe.py
@@ -0,0 +1,25 @@
+tictactoe = "____________________"
+
+print(tictactoe)
+
+#def evaluate(): 
+   # if tictactoe == "XXX":
+    #    return "X"
+   # elif tictactoe == "ooo":
+    #    return "o"
+   # elif tictactoe == 
+     #   return "!"
+   # elif tictactoe == 
+    #    return "-"
+    #pass
+
+#def move(board, mark, position):
+    #pass
+
+#def player_move(position, sign):
+ #   input("which position do you wannt to play between 0 and 19")
+  #  tictactoe_new = 
+
+def pc_move(board):
+   from random import randrange
+print("randrange"(19))
